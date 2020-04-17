cb24ef24aa59120fb29b124803961e1234e4e746
Mehmet <mehmetdolasan10@gmail.com>
2020-04-17 19:04:55 +0300

    Kullanıcı Giriş-Çıkış İşlemleri-2

--- app/src/main/java/com/mehmetdolasan/instagramcloneparseexample/SignUpActivity.java
+++ app/src/main/java/com/mehmetdolasan/instagramcloneparseexample/SignUpActivity.java
@@ -2,12 +2,18 @@ package com.mehmetdolasan.instagramcloneparseexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -25,14 +31,47 @@ public class SignUpActivity extends AppCompatActivity {
         signIn = findViewById(R.id.sign_up_activity_sign_in_button);
         signUp = findViewById(R.id.sign_up_activity_sign_up_button);
 
+        ParseUser parseUser = ParseUser.getCurrentUser();
+        if(parseUser != null){
+            Intent intent = new Intent(getApplicationContext(),FeedActivity.class);
+            startActivity(intent);
+        }
 
     }
 
     public void signIn(View view){
-
+        ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Welcome "+user.getUsername(), Toast.LENGTH_LONG).show();
+                    //intent
+                    Intent intent = new Intent(getApplicationContext(),FeedActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     public void signUp(View view){
+        ParseUser user = new ParseUser();
+        user.setUsername(usernameText.getText().toString());
+        user.setPassword(passwordText.getText().toString());
 
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "User Created ! ! !", Toast.LENGTH_LONG).show();
+                    //Intent
+                    Intent intent = new Intent(getApplicationContext(),FeedActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
